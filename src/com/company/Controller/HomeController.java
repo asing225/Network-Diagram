73621e66b63ecfc13cc1350581b13408ad60fed8
bhawana prasad <bprasad1.asu.edu>
2018-10-20 16:43:02 -0700

    Implemented the business logic for different scenarios.

--- src/com/company/Controller/HomeController.java
+++ src/com/company/Controller/HomeController.java
@@ -1,30 +1,22 @@
 package com.company.Controller;
 
-
+import com.company.Main;
 import com.company.Model.Activity;
-import com.company.Model.Business;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.controlsfx.control.CheckComboBox;
 
-import java.net.URL;
 import java.util.ArrayList;
 
-import java.util.ResourceBundle;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 
-public class HomeController extends Control implements Initializable {
+public class HomeController {
 
     //getting textFields data from user inputs in local variables.
     @FXML
@@ -32,27 +24,23 @@ public class HomeController extends Control implements Initializable {
     @FXML
     private TextField duration;
     @FXML
-    private TableColumn<String, String> activityColumn;
+    private TextField dependency;
     @FXML
-    private TableColumn<String, String> durationColumn;
-    @FXML
-    private TableColumn<String, String> dependencyColumn;
-    @FXML
-    private TableView<String> activityTable;
+    private TextArea finalOutput;
     @FXML
     private Label errorActivity;
     @FXML
     private Label errorDuration;
     @FXML
     private Label errorDependency;
-    @FXML
-    private CheckComboBox<String> dropDown = new CheckComboBox<String>();
+    /*@FXML
+    private CheckComboBox<String> dropDown = new CheckComboBox<String>();*/
 
     //local variables to store the user data.
     private ArrayList<String> inputActivity = new ArrayList();
 
     private List<Activity> activityList = new ArrayList<Activity>();
-    private List<Map.Entry<List<String>, Integer>> Output = new ArrayList<Map.Entry<List<String>, Integer>>();
+    private List<Map.Entry<List<String>, Integer>> output = new ArrayList<Map.Entry<List<String>, Integer>>();
 
 
     @FXML
@@ -60,51 +48,51 @@ public class HomeController extends Control implements Initializable {
     public void addActivity(ActionEvent event) {
         activityName.getStyleClass().remove("error");       //removing the error styling of red textfields
         duration.getStyleClass().remove("error");
-        dropDown.getStyleClass().remove("error");
+        dependency.getStyleClass().remove("error");
         errorActivity.setText("");                              //setting the page to reset after each activity is added.
         errorDuration.setText("");
         errorDependency.setText("");
 
+        if(dependency.getText().isEmpty()){                                //Setting up first activity as a root element.
+            dependency.setText("root");
+            errorDependency.setText("This is a root Activity");
+            errorDependency.setStyle("-fx-text-fill: black;");
+
+        }
+
         //Error handling of input fields
-        if (activityName.getText().isEmpty() || duration.getText().isEmpty() || duration.getText().equals("0") || !duration.getText().matches("\\d*") || dropDown.getCheckModel().isEmpty()) {
-            if (activityName.getText().isEmpty()) {
+        if(activityName.getText().isEmpty()||duration.getText().isEmpty()||dependency.getText().isEmpty()|| !duration.getText().matches("\\d*")){
+            if(activityName.getText().isEmpty()) {
                 activityName.getStyleClass().add("error");
                 errorActivity.setText("Please enter Activity Name");
             }
-            if (duration.getText().isEmpty()) {
+            if(duration.getText().isEmpty()) {
                 duration.getStyleClass().add("error");
                 errorDuration.setText("Please enter Duration");
             }
-            if (!duration.getText().matches("\\d*")) {
+            if(dependency.getText().isEmpty()) {
+                dependency.getStyleClass().add("error");
+                errorDependency.setText("Please enter Dependency");
+            }
+            if(!duration.getText().matches("\\d*")){
                 duration.getStyleClass().add("error");
                 errorDuration.setText("Please enter only numeric values");
-            }
-            if (duration.getText().equals("0")) {
-                duration.getStyleClass().add("error");
-                errorDuration.setText("Duration of an activity cannot be 0");
-            }
-            if (dropDown.getCheckModel().isEmpty()) {
-                errorDependency.setText("Please select a dependency");
-                dropDown.getStyleClass().add("error");
             }
             return;
         } else {
 
-            inputActivity.add(activityName.getText());
             Activity activity = new Activity();
             activity.setActivity(activityName.getText());
             activity.setDuration(Integer.parseInt(duration.getText()));
-            activity.setDependency((String.valueOf(dropDown.getCheckModel().getCheckedItems())).replaceAll("\\[","").replaceAll("\\]",""));
+            activity.setDependency(dependency.getText());
+            finalOutput.setText("Activity "+ activityName.getText() + " added with Duration: "+ duration.getText());
+            finalOutput.appendText("\n");
             activityList.add(activity);
-            resetActivity(event);
         }
+            resetActivity(event);
     }
 
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        dropDown.getItems().add(String.valueOf("Root"));
-    }
 
     @FXML
     public void resetActivity(ActionEvent event) {
@@ -114,15 +102,9 @@ public class HomeController extends Control implements Initializable {
         duration.clear();
         errorDuration.setText("");
         duration.getStyleClass().remove("error");
+        dependency.clear();
         errorDependency.setText("");
-        dropDown.getStyleClass().remove("error");
-        if (!inputActivity.get(0).isEmpty()) {
-            dropDown.getItems().clear();
-        }
-        ObservableList<String> list = FXCollections.observableArrayList();
-        list.addAll(inputActivity);
-        dropDown.getItems().addAll(list);
-        dropDown.getCheckModel().clearChecks();
+        dependency.getStyleClass().remove("error");
     }
 
     @FXML                                                       //method to open About window on button click.
@@ -153,12 +135,31 @@ public class HomeController extends Control implements Initializable {
 
     @FXML
     public void createNetworkDiagram(ActionEvent event) throws Exception {
-        FinalPage window = new FinalPage();
-        Stage newWin = window.finalPage();
-        newWin.setX(300);
-        newWin.setY(100);
-        newWin.show();
-        Business path = new Business();
-        Output = path.createNetwork(activityList);
+        if(activityList.size()==0){
+            finalOutput.setText("Please enter at least 1 Activity.");
+            return;
+        }
+        else{
+            Business path = new Business();
+            output = path.createNetwork(activityList);
+            if(output==null){
+                finalOutput.setText("The Network Diagram should not be cyclic.");
+            }
+            else{
+                Object[] pathsToArrays = output.toArray();
+                finalOutput.setText(null);
+                for(int i=0;i<pathsToArrays.length;i++){
+                    String paths = pathsToArrays[i].toString().replaceAll("\\[","").replaceAll("\\]","").replaceAll(",","->").replaceAll(" ","");
+                    String pathDuration = paths.substring(paths.lastIndexOf("=")).replaceAll("=","");
+                    finalOutput.appendText("\n" + "Path: "+ paths.substring(0,paths.indexOf("=")) +"\n" + "Duration: "+ pathDuration+ "\n");
+                }
+            }
+        }
+    }
+
+    public void newNetworkDiagram(ActionEvent event) throws Exception {
+        Main main = new Main();
+        Stage primaryStage = new Stage();
+        main.start(primaryStage);
     }
 }
