69b07215a1904bc095dc33d138088e10c1e0089d
[Aman] <amanjot5552@gmail.com>
2018-10-25 17:26:21 -0700

    Implemented the business logic for different scenarios.

--- src/com/company/Controller/HomeController.java
+++ src/com/company/Controller/HomeController.java
@@ -1,10 +1,8 @@
 package com.company.Controller;
 
-
 import com.company.Main;
 import com.company.Model.Activity;
 import com.company.Model.Business;
-
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,90 +12,145 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.cell.TextFieldTreeTableCell;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
-import org.controlsfx.control.CheckComboBox;
+import javafx.util.converter.IntegerStringConverter;
+
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
-public class HomeController extends Control implements Initializable {
+public class HomeController implements Initializable {
 
-    //getting textFields data from user inputs in local variables.
     @FXML
-    private TextArea finalOutput;
+    public TableView<Activity> activityTable;
     @FXML
     private TextField activityName;
     @FXML
     private TextField duration;
+    @FXML
+    private TextField dependency;
+    @FXML
+    private TextArea finalOutput;
     @FXML
     private Label errorActivity;
     @FXML
     private Label errorDuration;
     @FXML
     private Label errorDependency;
-    @FXML
-    private CheckComboBox<String> dropDown = new CheckComboBox<String>();
 
-    //local variables to store the user data.
     private ArrayList<String> inputActivity = new ArrayList();
-
     private List<Activity> activityList = new ArrayList<Activity>();
     private List<Map.Entry<List<String>, Integer>> output = new ArrayList<Map.Entry<List<String>, Integer>>();
+
+    private TableColumn<Activity, String> activityNameColumn = new TableColumn<Activity, String>("Activity Name");
+    private TableColumn<Activity, Integer> durationColumn = new TableColumn<Activity, Integer>("Duration");
+    private TableColumn<Activity, String> dependencyColumn = new TableColumn<Activity, String>("Dependency");
+
 
     @FXML
     //method to add the activity entered by user to our local variables
     public void addActivity(ActionEvent event) {
         activityName.getStyleClass().remove("error");       //removing the error styling of red textfields
         duration.getStyleClass().remove("error");
-        dropDown.getStyleClass().remove("error");
+        dependency.getStyleClass().remove("error");
         errorActivity.setText("");                              //setting the page to reset after each activity is added.
         errorDuration.setText("");
         errorDependency.setText("");
 
+        if(dependency.getText().isEmpty()){                                //Setting up first activity as a root element.
+            dependency.setText("root");
+            errorDependency.setText("This is a root Activity");
+            errorDependency.setStyle("-fx-text-fill: black;");
+
+        }
+
         //Error handling of input fields
-        if (activityName.getText().isEmpty() || duration.getText().isEmpty() || duration.getText().equals("0") || !duration.getText().matches("\\d*") || dropDown.getCheckModel().isEmpty()) {
-            if (activityName.getText().isEmpty()) {
+        if(activityName.getText().isEmpty()||duration.getText().isEmpty()||dependency.getText().isEmpty()|| !duration.getText().matches("\\d*")){
+            if(activityName.getText().isEmpty()) {
                 activityName.getStyleClass().add("error");
                 errorActivity.setText("Please enter Activity Name");
             }
-            if (duration.getText().isEmpty()) {
+            if(duration.getText().isEmpty()) {
                 duration.getStyleClass().add("error");
                 errorDuration.setText("Please enter Duration");
             }
-            if (!duration.getText().matches("\\d*")) {
+            if(dependency.getText().isEmpty()) {
+                dependency.getStyleClass().add("error");
+                errorDependency.setText("Please enter Dependency");
+            }
+            if(!duration.getText().matches("\\d*")){
                 duration.getStyleClass().add("error");
                 errorDuration.setText("Please enter only numeric values");
-            }
-            if (duration.getText().equals("0")) {
-                duration.getStyleClass().add("error");
-                errorDuration.setText("Duration of an activity cannot be 0");
-            }
-            if (dropDown.getCheckModel().isEmpty()) {
-                errorDependency.setText("Please select a dependency");
-                dropDown.getStyleClass().add("error");
             }
             return;
         } else {
 
-            inputActivity.add(activityName.getText());
             Activity activity = new Activity();
             activity.setActivity(activityName.getText());
             activity.setDuration(Integer.parseInt(duration.getText()));
-            activity.setDependency((String.valueOf(dropDown.getCheckModel().getCheckedItems())).replaceAll("\\[","").replaceAll("\\]",""));
-            activityList.add(activity);
+            activity.setDependency(dependency.getText());
             finalOutput.setText("Activity "+ activityName.getText() + " added with Duration: "+ duration.getText());
             finalOutput.appendText("\n");
-            resetActivity(event);
+            activityList.add(activity);
+            ////////
+            ObservableList<Activity> data = FXCollections.observableArrayList(activityList);
+            activityTable.setItems(data);
         }
+            resetActivity(event);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        dropDown.getItems().add(String.valueOf("Root"));
+        activityTable.setEditable(true);
+        activityNameColumn.setCellValueFactory(new PropertyValueFactory("activity"));
+        activityNameColumn.setMinWidth(250);
+        activityNameColumn.setCellFactory(TextFieldTableCell.<Activity>forTableColumn());
+        editableActivityColumn();
+
+        durationColumn.setCellValueFactory(new PropertyValueFactory("duration"));
+        durationColumn.setMinWidth(250);
+        durationColumn.setCellFactory(TextFieldTableCell.<Activity, Integer>forTableColumn(new IntegerStringConverter()));
+        editableDurationColumn();
+
+        dependencyColumn.setCellValueFactory(new PropertyValueFactory("dependency"));
+        dependencyColumn.setMinWidth(250);
+        dependencyColumn.setCellFactory(TextFieldTableCell.<Activity>forTableColumn());
+        editableDependencyColumn();
+
+        activityTable.getColumns().addAll(activityNameColumn, durationColumn, dependencyColumn);
     }
+
+    private void editableActivityColumn(){
+        activityNameColumn.setOnEditCommit((TableColumn.CellEditEvent<Activity, String> event) -> {
+            final String name = event.getNewValue() != null ? event.getNewValue()
+                    : event.getOldValue();
+            ((Activity) event.getTableView().getItems().get(event.getTablePosition().getRow())).setActivity(name);
+            activityTable.refresh();
+        });
+    }
+
+    private void editableDurationColumn(){
+        durationColumn.setOnEditCommit((TableColumn.CellEditEvent<Activity, Integer> event) -> {
+            final int time = event.getNewValue() != null ? event.getNewValue()
+                    : event.getOldValue();
+            ((Activity) event.getTableView().getItems().get(event.getTablePosition().getRow())).setDuration(time);
+            activityTable.refresh();
+        });
+    }
+
+    private void editableDependencyColumn(){
+        dependencyColumn.setOnEditCommit((TableColumn.CellEditEvent<Activity, String> event) -> {
+            final String depend = event.getNewValue() != null ? event.getNewValue()
+                    : event.getOldValue();
+            ((Activity) event.getTableView().getItems().get(event.getTablePosition().getRow())).setDependency(depend);
+            activityTable.refresh();
+        });
+    }
+
 
     @FXML
     public void resetActivity(ActionEvent event) {
@@ -107,15 +160,9 @@ public class HomeController extends Control implements Initializable {
         duration.clear();
         errorDuration.setText("");
         duration.getStyleClass().remove("error");
+        dependency.clear();
         errorDependency.setText("");
-        dropDown.getStyleClass().remove("error");
-        if(!activityList.isEmpty()){
-            dropDown.getItems().clear();
-        }
-        ObservableList<String> list = FXCollections.observableArrayList();
-        list.addAll(inputActivity);
-        dropDown.getItems().addAll(list);
-        dropDown.getCheckModel().clearChecks();
+        dependency.getStyleClass().remove("error");
     }
 
     @FXML                                                       //method to open About window on button click.
@@ -172,5 +219,9 @@ public class HomeController extends Control implements Initializable {
         Main main = new Main();
         Stage primaryStage = new Stage();
         main.start(primaryStage);
+    }
+
+    public void updateDiagram(ActionEvent event) throws Exception {
+        createNetworkDiagram(event);
     }
 }
